a961c3990513ace4ec1c98c240064e80ef2531a5
Douglas Hipolito <me@douglashipolito.com>
2014-10-01 00:28:16 -0300

    adding all required methods

--- src/main.js
+++ src/main.js
@@ -54,7 +54,63 @@
       return o;
     }
 
-    this.selector = selector;
+    instances.push(currentInstance);
+    currentInstance.instanceIndex = instances.length - 1;
+    currentInstance.selector = selector;
+    currentInstance.element = [];
+    currentInstance._debug = false;
+    currentInstance.currentVisibility = 'unknown';
+
+    currentInstance.events = {
+      appear: {
+        fn: null,
+        count: 0,
+        action: 'appeared'
+      },
+      disappear: {
+        fn: null,
+        count: 0,
+        action: 'disappeared'
+      },
+      insert: {
+        fn: null,
+        count: 0,
+        action: 'inserted'
+      },
+      remove: {
+        fn: null,
+        count: 0,
+        action: 'removed'
+      }
+    };
+
+    currentInstance.observer = getObserver.call(currentInstance, selector);
+
+    //If the second is a function, it will treated as callback of appear observer
+    if(options && typeof options === 'function') {
+      currentInstance.observer.observe('appear', options);
+    }
+
+    //Check if there are options
+    if(Object.prototype.toString.call(options) === '[object Object]') {
+
+      //Set debug option
+      if(options.debug) {
+        currentInstance._debug = options.debug;
+      }
+
+      //Set the context to find, in the case of Insert and Remove Observer
+      if(options.context) {
+        checkContext.call(currentInstance, options.context);
+      }
+
+      //Loop through events and attach all that have been defined
+      util.forEach.call(Object.keys(currentInstance.events), function (eventKey) {
+        if(typeof options[eventKey] === 'function') {
+          currentInstance.observer.observe(eventKey, options[eventKey]);
+        }
+      });
+    }
   }
 
   //FollowElement prototype
@@ -71,7 +127,11 @@
      * @return {followElement} returns this current instance
      */
     connect: function (event, callback) {
-      console.log(event, callback);
+      if(!this.events[event]) {
+        return false;
+      }
+
+      this.observer.observe(event, callback);
       return this;
     },
 
@@ -81,7 +141,7 @@
      * @return {followElement} returns this current instance
      */
     disconnect: function () {
-      console.log('disconnect');
+      disconnectObservers.call(this);
       return this;
     },
 
@@ -96,7 +156,7 @@
      * @return {followElement} returns this current instance
      */
     insert: function (callback, context) {
-      console.log(callback, context);
+      this.observer.observe('insert', callback, context);
       return this;
     },
 
@@ -110,7 +170,7 @@
      * @return {followElement} returns this current instance
      */
     remove: function (callback, context) {
-      console.log(callback, context);
+      this.observer.observe('remove', callback, context);
       return this;
     },
 
@@ -123,7 +183,7 @@
      * @return {followElement} returns this current instance
      */
     appear: function (callback) {
-      console.log(callback);
+      this.observer.observe('appear', callback);
       return this;
     },
 
@@ -136,7 +196,7 @@
      * @return {followElement} returns this current instance
      */
     disappear: function (callback) {
-      console.log(callback);
+      this.observer.observe('disappear', callback);
       return this;
     },
 
@@ -146,7 +206,7 @@
      * @return {followElement} returns this current instance
      */
     pause: function () {
-      console.log('pause');
+      this.observer.paused = true;
       return this;
     },
 
@@ -156,7 +216,7 @@
      * @return {followElement} returns this current instance
      */
     resume: function () {
-      console.log('resume');
+      this.observer.paused = false;
       return this;
     },
 
@@ -166,7 +226,7 @@
      * @return {followElement} returns this current instance
      */
     debug: function () {
-      console.log('debug');
+      this._debug = !this._debug;
       return this;
     },
 
@@ -179,10 +239,424 @@
      * @return {followElement} returns this current instance
      */
     setContext: function (context) {
-      console.log(context);
+      checkContext.call(this, context);
       return this;
     }
   };
+
+  /**
+   * [checkContext description]
+   * @param  {[type]} context [description]
+   * @param  {[type]} type    [description]
+   * @return {[type]}         [description]
+   */
+  function checkContext(context, type) {
+    var currentInstance = this;
+
+    if(!context && !currentInstance.context) {
+      throw new Error(logSignature(this.selector, type, 'You must pass the "context" param'));
+    }
+
+    if(context && context.nodeType !== 1) {
+      context = util.find(context);
+    }
+
+    if(!currentInstance.context && context) {
+      currentInstance.context = context;
+    }
+
+    return currentInstance.context;
+  }
+
+  /**
+   * [disconnectObservers description]
+   * @return {[type]} [description]
+   */
+  function disconnectObservers() {
+    var currentInstance = this,
+        styleElement = util.findAll('style.' + getStylesClass.call(currentInstance, currentInstance.selector));
+
+    //disconnect the mutation observer
+    if(currentInstance.observer._MutationObserverInstance) {
+      currentInstance.observer._MutationObserverInstance.disconnect();
+    }
+
+    util.forEach.call(styleElement, function (element) {
+      element.parentElement.removeChild(element);
+    });
+
+    currentInstance.observer.disconnected = true;
+  }
+
+  /**
+   * [getObserver description]
+   * @param  {[type]} selector [description]
+   * @return {[type]}          [description]
+   */
+  function getObserver(selector) {
+    var currentInstance = this,
+        _observer;
+
+    _observer = {
+      appear: function () {
+        appearedObserver.call(this, selector);
+      },
+
+      disappear: function () {
+        checkDisappear.call(currentInstance);
+      },
+
+      insert: function (context) {
+        context = checkContext.call(currentInstance, context, 'insert');
+        insertObserver.call(currentInstance, context);
+      },
+
+      remove: function (context) {
+        context = checkContext.call(currentInstance, context, 'remove');
+        checkRemove.call(currentInstance, context);
+      }
+    };
+
+    return {
+      paused: false,
+
+      observe: function (event, callback, context) {
+        if(currentInstance.events[event]) {
+          currentInstance.events[event].fn = callback;
+
+          if(_observer[event]) {
+            _observer[event].call(currentInstance, context);
+          }
+        }
+      }
+
+    };
+  }
+
+  /**
+   * [elementMatch description]
+   * @param  {[type]} element [description]
+   * @param  {[type]} type    [description]
+   * @return {[type]}         [description]
+   */
+  function elementMatch(element, type) {
+    var currentInstance = this,
+        currentType;
+
+    if(currentInstance.observer.paused) {
+      return false;
+    }
+
+    if (util.matches.call(element, currentInstance.selector)) {
+      currentType = currentInstance.events[type];
+
+      if(typeof currentType.fn === 'function') {
+
+        if(!element.__followElement) {
+          currentInstance.element.push(element);
+          element.__followElement = true;
+        }
+
+        currentType.fn.call(currentInstance, element);
+        currentType.count++;
+        currentInstance.currentVisibility = type;
+
+        if(currentInstance._debug) {
+          console.log(logSignature(this.selector, type, 'this element has ' + currentType.action));
+        }
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * [insertObserver description]
+   * @param  {[type]} context [description]
+   * @return {[type]}         [description]
+   */
+  function insertObserver(context) {
+    var currentInstance = this,
+        currentElements = util.findAll(this.selector);
+
+    util.forEach.call(currentElements, function (currentElement) {
+      elementMatch.apply(currentInstance, [currentElement, 'insert']);
+    });
+
+    if(canIUseMutationObserver()) {
+      currentInstance.observer._MutationObserverInstance = setMutationObserver.call(currentInstance, context);
+    } else {
+      currentInstance.observer._MutationObserverInstance = setMutationObserverFallback.call(currentInstance, context);
+    }
+  }
+
+  /**
+   * [setMutationObserver description]
+   * @param {[type]} context [description]
+   */
+  function setMutationObserver(context) {
+    var currentInstance = this,
+        MutationObserverInstance;
+
+    MutationObserverInstance = new MutationObserver(function (mutations) {
+
+      mutations.forEach(function(mutation) {
+        util.forEach.call(mutation.addedNodes, function (node) {
+          if(node.nodeType !== 1) {
+            return;
+          }
+
+          elementMatch.apply(currentInstance, [node, 'insert']);
+        });
+
+        //On element removed
+        util.forEach.call(mutation.removedNodes, function (node) {
+          if(node.nodeType !== 1) {
+            return;
+          }
+
+          if(currentInstance.observer._checkRemove) {
+            elementMatch.apply(currentInstance, [node, 'remove']);
+          }
+        });
+      });
+    });
+
+    MutationObserverInstance.observe(context, {
+        childList: true
+    });
+
+    return MutationObserverInstance;
+  }
+
+  /**
+   * [setMutationObserverFallback description]
+   * @param {[type]} context [description]
+   */
+  function setMutationObserverFallback(context) {
+    var currentInstance = this,
+        insertedListener = function (event) {
+          var target = event.target;
+          elementMatch.apply(currentInstance, [target, 'insert']);
+        },
+        removedListener = function (event) {
+          var target = event.target;
+
+          if(currentInstance.observer._checkRemove) {
+            elementMatch.apply(currentInstance, [target, 'remove']);
+          }
+        };
+
+    if(!currentInstance.observer._listeningDOMNodeInserted) {
+      context.addEventListener('DOMNodeInserted', insertedListener);
+      currentInstance.observer._listeningDOMNodeInserted = true;
+    }
+
+    if(!currentInstance.observer._listeningDOMNodeRemoved) {
+      context.addEventListener('DOMNodeRemoved', removedListener);
+      currentInstance.observer._listeningDOMNodeRemoved = true;
+    }
+
+    return {
+      disconnect: function () {
+        context.removeEventListener('DOMNodeInserted', insertedListener, false);
+        context.removeEventListener('DOMNodeRemoved', removedListener, false);
+      }
+    };
+  }
+
+  /**
+   * [canIUseMutationObserver description]
+   * @return {[type]} [description]
+   */
+  function canIUseMutationObserver() {
+    var canIUse = !!MutationObserver;
+
+    //There is a issue with childList on iOS 6
+    if(/(iPhone|iPad|iPod)\sOS\s6/.test(root.navigator.userAgent)) {
+        canIUse = false;
+    }
+
+    return canIUse;
+  }
+
+  /**
+   * [checkRemove description]
+   * @return {[type]} [description]
+   */
+  function checkRemove() {
+    var instance = this;
+
+    instance.observer._checkRemove = true;
+  }
+
+  /**
+   * [appearedObserver description]
+   * @param  {[type]} selector [description]
+   * @return {[type]}          [description]
+   */
+  function appearedObserver(selector) {
+    var events = ['animationstart', 'webkitAnimationStart', 'oanimationstart', 'MSAnimationStart'],
+        options = {
+          keyframes: getKeyframeString(),
+          selector: selector,
+          stylesClass: getStylesClass.call(this, selector),
+          styles: getAnimationNameString(selector)
+        },
+        head = util.find('head'),
+        createElement = function (elementName, options) {
+          var element = document.createElement(elementName);
+
+          if(options) {
+
+            if(options.className) {
+              element.className += ' ' + options.className;
+            }
+
+            if(options.content) {
+              element.innerText = options.content;
+            }
+          }
+
+          return element;
+        };
+
+    // if the keyframes aren't present, add them in a style element
+    if(!util.findAll("style.domnodeappear-keyframes").length) {
+      head.appendChild(
+          createElement('style', {
+            className: 'domnodeappear-keyframes',
+            content: options.keyframes
+          })
+      );
+    }
+
+    if(!util.findAll('style.' + options.stylesClass).length) {
+      // add animation to selected element
+      head.appendChild(
+        createElement('style', {
+          className: options.stylesClass,
+          content: options.styles
+        })
+      );
+    }
+
+    if(!cssObserverStarted) {
+      util.forEach.call(events, function (eventName) {
+
+        // on animation start, execute the callback
+        document.addEventListener(eventName, function (event) {
+          var element = event.target;
+
+          if(event.animationName !== 'elementAppeared') {
+            return;
+          }
+
+          util.forEach.call(instances, function (instance) {
+            if(!elementIsHidden(element)) {
+              elementMatch.apply(instance, [element, 'appear']);
+            }
+
+            //This is the element that we want
+            if(util.matches.call(element, instance.selector)) {
+
+              //Only start the disappear observer if this element has appeared
+              //and this instance wants to be notified when this element disappear
+              if(instance.observer._checkDisappear && instance.events.appear.count) {
+                disappearedObserver.call(instance, element);
+              }
+            }
+          });
+
+        }, false);
+      });
+
+      cssObserverStarted = true;
+    }
+  }
+
+  /**
+   * [checkDisappear description]
+   * @return {[type]} [description]
+   */
+  function checkDisappear() {
+    var instance = this;
+
+    instance.observer._checkDisappear = true;
+  }
+
+  /**
+   * [disappearedObserver description]
+   * @param  {[type]} element [description]
+   * @return {[type]}         [description]
+   */
+  function disappearedObserver(element) {
+    var instance = this,
+        timeoutId,
+        timeoutTime = 160;
+
+    timeoutId = setTimeout(function checkVisibility() {
+      if(elementIsHidden(element)) {
+        clearTimeout(timeoutId);
+
+        elementMatch.apply(instance, [element, 'disappear']);
+        return;
+      }
+
+      setTimeout(checkVisibility, timeoutTime);
+    }, timeoutTime);
+  }
+
+  /**
+   * [elementIsHidden description]
+   * @param  {[type]} element [description]
+   * @return {[type]}         [description]
+   */
+  function elementIsHidden(element) {
+    return element !== null
+          && (element.offsetWidth === 0 && element.offsetHeight === 0)
+          || element.offsetParent === null;
+  }
+
+  /**
+   * [getKeyframeString description]
+   * @return {[type]} [description]
+   */
+  function getKeyframeString() {
+    var baseKeyframe = ' elementAppeared { from { outline: 1px solid transparent; }' +
+                                        '  to { outline: 0px solid transparent; } }',
+        keyframeString = '';
+
+    util.forEach.call(['@keyframes', '@-moz-keyframes', '@-webkit-keyframes', '@-ms-keyframes', '@-o-keyframes'],
+      function (prefix) {
+        keyframeString += prefix + baseKeyframe + ' ';
+      });
+
+    return keyframeString;
+  }
+
+  /**
+   * [getStylesClass description]
+   * @param  {[type]} selector [description]
+   * @return {[type]}          [description]
+   */
+  function getStylesClass(selector) {
+    return selector.replace(".", "") + '-animation-' + this.instanceIndex;
+  }
+
+  /**
+   * [getAnimationNameString description]
+   * @param  {[type]} selector [description]
+   * @return {[type]}          [description]
+   */
+  function getAnimationNameString(selector) {
+    return selector + ' { animation-name: elementAppeared;' +
+             '-webkit-animation-name: elementAppeared;' +
+             'animation-duration: 0.001s;' +
+             '-webkit-animation-duration: 0.001s; }';
+  }
 
   /**
    * Simple signature for logs
